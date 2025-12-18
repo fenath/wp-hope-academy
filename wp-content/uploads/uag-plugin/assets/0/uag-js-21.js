7d52a47022c4127ccff4888e627e28863323850f
Fernando Almeida <fernando@macpg.com>
2025-12-17 23:13:34 -0300

    initial commit

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-21.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"a7d0faf6","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"dev-email@wpengine.local","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-a7d0faf6', 21 );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-58.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"07b24085","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-07b24085' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"07b24085","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-07b24085' );
+});
+ });
